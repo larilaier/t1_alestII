dc95223e5d397768832dcde46839bb787bdcc50f
larilaier <lari.laier@hotmail.com>
2023-08-19 16:32:59 -0300

    atualizado

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        cadeiaDNA cadeiaRecebida = new cadeiaDNA();
+        System.out.println("Insira a cadeia: ");
+        //só aceitar caracteres D N e A 
+        //fazer toUpperCase
+        cadeiaRecebida = t.nextLine();
+        StringBuilder cadeiaNova = new StringBuilder();
+
+
+        System.out.println("Cadeia Original: " + cadeiaOriginal);
+        System.out.println("Cadeia Nova: " + cadeiaNova);
+    }
+}
--- cadeiaDNA.java
+++ cadeiaDNA.java
@@ -1,60 +1,54 @@
 import java.util.Scanner;
 
-public class cadeiaDNA{
-    String cadeia;
-    Scanner t = new Scanner(System.in);
-
-    System.out.println("Insira a cadeia: ");
-    cadeia = t.nextLine();
-
-
+public class cadeiaDNA {
+    static String cadeia;
+    static String cadeiaNova;
     char recebido;
     char D;
     char N;
     char A;
+    static Scanner t = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        cadeiaDNA analiseDNA = new cadeiaDNA();
-        analiseDNA.analisarCadeia();
+    public cadeiaDNA() {
+
     }
-
-    
 
     public void analisarCadeia() {
 
-        for (int i=0; i<cadeia.length(); i++) {
-        char currentChar = cadeia.charAt(i);
-        if (i < cadeia.length() - 1) {
-            char nextChar = cadeia.charAt(i + 1);
-            if (currentChar == nextChar) {
-                i++;
-                continue;
+        for (int i = 0; i < cadeia.length(); i++) {
+            char currentChar = cadeia.charAt(i);
+            if (i < cadeia.length() - 1) {
+                char nextChar = cadeia.charAt(i + 1);
+                if (currentChar == nextChar) {
+                    i++;
+                    continue;
+                }
             }
         }
-    }
 
-        for (int i = 0; i < cadeia.length(); i++) {
+
+        /*for (int i = 0; i < cadeia.length(); i++) {
             char caractereAtual = cadeia.charAt(i);
             switch (caractereAtual) {
                 case 'A':
                     if (recebido == 'D' + 'N' || recebido == 'N' + 'D') {
-                        System.out.println("A" + cadeia);
+                        System.out.println(cadeiaNova + "A" + cadeia);
                     }
                     break;
                 case 'D':
                     if (recebido == 'N' + 'A' || recebido == 'A' + 'N') {
-                        System.out.println("D" + cadeia);
+                        System.out.println(cadeiaNova + "D" + cadeia);
                     }
                     break;
                 case 'N':
                     if (recebido == 'D' + 'A' || recebido == 'A' + 'D') {
-                        System.out.println("N" + cadeia);
+                        System.out.println(cadeiaNova + "N" + cadeia);
                     }
                     break;
                 default:
                     // passar para o próximo caractere da string
                     break;
-            }
+            }**/
         }
 
     }
